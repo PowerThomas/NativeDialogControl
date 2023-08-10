20aa9078e3f6095f4c083427a12f0dbeeac2aec9
Thomas van der Waard <t.van.der.waard@indocs.nl>
2023-08-10 15:57:52 +0200

    Included output event to simulate the OnSelect.

---
+++ NativeDialogControl/IControlEvent.ts
@@ -0,0 +1,4 @@
+export interface IControlEvent {
+    eventName: 'OnSelect';
+    eventValue?: string;
+}
--- NativeDialogControl/index.ts
+++ NativeDialogControl/index.ts
@@ -1,11 +1,15 @@
-import {IInputs, IOutputs} from "./generated/ManifestTypes";
+import { IInputs, IOutputs } from "./generated/ManifestTypes";
+import { IControlEvent } from "./IControlEvent";
 
 export class NativeDialogControl implements ComponentFramework.StandardControl<IInputs, IOutputs> {
     // PCF framework delegate which will be assigned to this object which would be called whenever any update happens. 
     private _notifyOutputChanged: () => void;
 
+    // Reference to the event that will be fired.
+    private _event: IControlEvent;
+
     // Reference to the div element that hold together all the HTML elements that we are creating as part of this control
-    private _container: HTMLDivElement;
+    //private _container: HTMLDivElement;
 
     // Reference to ComponentFramework Context object
     private _context: ComponentFramework.Context<IInputs>;
@@ -13,11 +17,11 @@ export class NativeDialogControl implements ComponentFramework.StandardControl<I
     // Reference to the current open state of the dialog
     private _dialogIsVisible: boolean;
 
+
     /**
      * Empty constructor.
      */
-    constructor()
-    {
+    constructor() {
 
     }
 
@@ -29,8 +33,7 @@ export class NativeDialogControl implements ComponentFramework.StandardControl<I
      * @param state A piece of data that persists in one session for a single user. Can be set at any point in a controls life cycle by calling 'setControlState' in the Mode interface.
      * @param container If a control is marked control-type='standard', it will receive an empty div element within which it can render its content.
      */
-    public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container:HTMLDivElement): void
-    {
+    public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container: HTMLDivElement): void {
         this._notifyOutputChanged = notifyOutputChanged;
         this._context = context;
     }
@@ -40,8 +43,7 @@ export class NativeDialogControl implements ComponentFramework.StandardControl<I
      * Called when any value in the property bag has changed. This includes field values, data-sets, global values such as container height and width, offline status, control metadata values such as label, visible, etc.
      * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to names defined in the manifest, as well as utility functions
      */
-    public updateView(context: ComponentFramework.Context<IInputs>): void
-    {
+    public updateView(context: ComponentFramework.Context<IInputs>): void {
         this._context = context;
         const p = this._context.parameters;
 
@@ -61,7 +63,11 @@ export class NativeDialogControl implements ComponentFramework.StandardControl<I
                     ).then(
                         () => {
                             console.log("Alert dialog closed");
-                            this._dialogIsVisible = false;
+                            this._event = {
+                                eventName: 'OnSelect',
+                                eventValue: 'close',
+                            };
+                            this._notifyOutputChanged();
                         },
                         () => {
                             console.log("Error in Alert Dialog");
@@ -86,17 +92,22 @@ export class NativeDialogControl implements ComponentFramework.StandardControl<I
                         }
                     ).then(
                         (success) => {
+                            console.log(success);
                             if (success.confirmed) {
-                                () => {
-                                    console.log("Ok button clicked.");
-                                    this._dialogIsVisible = false;
-                                }
+                                console.log("Ok button clicked.");
+                                this._event = {
+                                    eventName: 'OnSelect',
+                                    eventValue: 'confirm',
+                                };
+                                this._notifyOutputChanged();
                             }
                             else {
-                                () => {
-                                    console.log("Cancel or X button clicked.");
-                                    this._dialogIsVisible = false;
-                                }
+                                console.log("Cancel or X button clicked.");
+                                this._event = {
+                                    eventName: 'OnSelect',
+                                    eventValue: 'close',
+                                };
+                                this._notifyOutputChanged();
                             }
                         }
                     );
@@ -111,7 +122,11 @@ export class NativeDialogControl implements ComponentFramework.StandardControl<I
                     ).then(
                         () => {
                             console.log("Error dialog closed");
-                            this._dialogIsVisible = false;
+                            this._event = {
+                                eventName: 'OnSelect',
+                                eventValue: 'close',
+                            };
+                            this._notifyOutputChanged();
                         },
                         () => {
                             console.log("Error in Error Dialog");
@@ -127,17 +142,16 @@ export class NativeDialogControl implements ComponentFramework.StandardControl<I
      * It is called by the framework prior to a control receiving new data.
      * @returns an object based on nomenclature defined in manifest, expecting object[s] for property marked as “bound” or “output”
      */
-    public getOutputs(): IOutputs
-    {
-        return {};
+    public getOutputs(): IOutputs {
+        this._dialogIsVisible = false;
+        return this._event;
     }
 
     /**
      * Called when the control is to be removed from the DOM tree. Controls should use this call for cleanup.
      * i.e. cancelling any pending remote calls, removing listeners, etc.
      */
-    public destroy(): void
-    {
+    public destroy(): void {
         // Add code to cleanup control if necessary
     }
 }
